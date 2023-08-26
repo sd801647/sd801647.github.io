cca5485b2a838754fa8806bcedbd11c4064cdfbe
SUBHAMAY DHARA <63958713+sd801647@users.noreply.github.com>
2023-08-27 01:06:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const targetDate = new Date("2023-08-27T01:20:00").getTime(); // Set your target date and time here
+const timerElement = document.getElementById("countdown");
+const giftElement = document.getElementById("gift");
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    console.log(now);
+    const timeLeft = targetDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds
+
+    if (timeLeft <= 0) {
+        timerElement.innerHTML = "It's Time to reveal ! Sorry for late wishes...";
+        giftElement.classList.remove("hidden");
+    }
+}
+
+updateCountdown(); // Initial call
+setInterval(updateCountdown, 1000); // Update every second
+
+
+
