d0a3a381d5150bc9bbd9ce0c7512d2408e0c7bff
SUBHAMAY DHARA <63958713+sd801647@users.noreply.github.com>
2023-08-27 01:30:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("2023-08-27T01:25:00").getTime(); // Set your target date and time here
+const targetDate = new Date("2023-08-27T12:00:00").getTime(); // Set your target date and time here
 const timerElement = document.getElementById("countdown");
 const giftElement = document.getElementById("gift");
 
